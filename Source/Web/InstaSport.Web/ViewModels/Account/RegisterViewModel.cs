47732937255f1bf8a4cffca43d487851e584629b
dilyantraykov <dtraykov94@gmail.com>
2016-02-20 23:20:56 +0200

    Removed password requirements. Users now login and register with username instead of email.

--- Source/Web/InstaSport.Web/ViewModels/Account/RegisterViewModel.cs
+++ Source/Web/InstaSport.Web/ViewModels/Account/RegisterViewModel.cs
@@ -5,12 +5,17 @@
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
