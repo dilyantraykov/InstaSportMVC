49a098df95015355b3da4edd00edaddfd24c8a0f
dilyantraykov <dtraykov94@gmail.com>
2016-02-20 21:34:42 +0200

    Added Game and Rating models

---
+++ Source/Data/InstaSport.Data.Models/Game.cs
@@ -0,0 +1,30 @@
+﻿namespace InstaSport.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Common.Models;
+
+    public class Game : BaseModel<int>
+    {
+        public Game()
+        {
+            this.Players = new HashSet<User>();
+        }
+
+        [Required]
+        public int LocationId { get; set; }
+
+        public virtual Location Location { get; set; }
+
+        [Required]
+        public int SportId { get; set; }
+
+        public virtual Sport Sport { get; set; }
+
+        public int? MinPlayers { get; set; }
+
+        public int? MaxPlayers { get; set; }
+
+        public virtual ICollection<User> Players { get; set; }
+    }
+}
---
+++ Source/Data/InstaSport.Data.Models/Rating.cs
@@ -0,0 +1,11 @@
+﻿namespace InstaSport.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using InstaSport.Data.Common.Models;
+
+    public class Rating : BaseModel<int>
+    {
+        [Range(0, 10)]
+        public int Value { get; set; }
+    }
+}
