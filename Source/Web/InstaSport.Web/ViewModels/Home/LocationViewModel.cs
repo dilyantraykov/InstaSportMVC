471e854c1f2941173bdef23ee1c43794d010bce3
dilyantraykov <dtraykov94@gmail.com>
2016-02-21 21:09:16 +0200

    Partial view for locations added. Resolved controller conflict.

--- Source/Web/InstaSport.Web/ViewModels/Home/LocationViewModel.cs
+++ Source/Web/InstaSport.Web/ViewModels/Home/LocationViewModel.cs
@@ -21,7 +21,7 @@
             get
             {
                 IIdentifierProvider identifier = new IdentifierProvider();
-                return $"/Locations/{identifier.EncodeId(this.Id)}";
+                return $"/Location/{identifier.EncodeId(this.Id)}";
             }
         }
 
