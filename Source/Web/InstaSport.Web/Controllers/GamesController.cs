8044695b580e727755771c630e7991d8abcf96da
dilyantraykov <dtraykov94@gmail.com>
2016-02-21 15:18:55 +0200

    Added GameDetails page as well as PlayerViewModel

---
+++ Source/Web/InstaSport.Web/Controllers/GamesController.cs
@@ -0,0 +1,27 @@
+﻿namespace InstaSport.Web.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+    using Infrastructure.Mapping;
+    using InstaSport.Services.Data;
+    using ViewModels.Games;
+
+    public class GamesController : BaseController
+    {
+        private IGamesService games;
+
+        public GamesController(IGamesService games)
+        {
+            this.games = games;
+        }
+
+        public ActionResult Index()
+        {
+            var games = this.games.GetUpcoming()
+                .To<UpcomingGameViewModel>()
+                .Take(10);
+
+            return this.View(games);
+        }
+    }
+}
---
+++ Source/Web/InstaSport.Web/ViewModels/Games/UpcomingGameViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace InstaSport.Web.ViewModels.Games
+{
+    using System;
+    using System.Collections.Generic;
+    using AutoMapper;
+    using Infrastructure.Mapping;
+    using InstaSport.Data.Models;
+
+    public class UpcomingGameViewModel : IMapFrom<Game>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string LocationName { get; set; }
+
+        public string SportName { get; set; }
+
+        public DateTime StartingDateTime { get; set; }
+
+        public IEnumerable<PlayerViewModel> Players { get; set; }
+
+        public int? MaxPlayers { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<Game, UpcomingGameViewModel>()
+                .ForMember(
+                    x => x.LocationName,
+                    opt => opt.MapFrom(x => x.Location.Name))
+                .ForMember(
+                    x => x.SportName,
+                    opt => opt.MapFrom(x => x.Sport.Name));
+        }
+    }
+}
